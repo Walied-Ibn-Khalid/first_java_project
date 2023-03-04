298e3e51c614b8a07dfa50021abcb93507621bbf
Walied_Sharf <walied.khalid.a.i@gmail.com>
2023-03-04 19:51:50 +0100

    first code commit

---
+++ java_playground.java
@@ -0,0 +1,14 @@
+package our_test_folder;
+
+import java.awt.*;
+
+public class java_playground {
+
+    public static void main(String[] args) {
+
+        String string = "is it working the share thing";
+        System.out.println(string);
+        System.out.println("how is it workign");
+
+    }
+}
